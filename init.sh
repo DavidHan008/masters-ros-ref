b5937588fddadc96bf59aedb56d7c61bd3bdad11
Andrew Vo <voandrew.khoa@gmail.com>
2017-02-08 21:48:40 -0800

    users must source setup.bash manually

--- init.sh
+++ init.sh
@@ -4,5 +4,6 @@ cd src
 catkin_init_workspace
 cd ..
 catkin_make
-source devel/setup.bash
+# source devel/setup.bash ## doesn't seem to work here. call this manually
 echo "Initialization is done"
+echo "Remember to run: source devel/setup.bash"
